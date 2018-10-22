e67b1057a01f7f50ce555d4e7fcac66bc6cff4b0
Waheeb Agherdien <wahe3bru@gmail.com>
2018-10-22 15:21:49 +0200

    completednasic exercises

---
+++ Basics/ex1.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Write a shell script that prints "Shell Scripting is Fun!" to the screen.
+
+echo "Shell scripting is fun!"
---
+++ Basics/ex2.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+VAR="Scripting is fun!"
+
+echo "$VAR"
